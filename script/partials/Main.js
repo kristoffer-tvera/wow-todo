a51ceefcefca0c136e86e935a9b1192a665c0c68
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-06-11 22:38:56 +0200

    First commit with boilerplate added

---
+++ script/partials/Main.js
@@ -0,0 +1,25 @@
+var classes = [];
+classes[0] = { name: 'Warrior', color: '#C79C6E' };
+classes[1] = { name: 'Paladin', color: '#F58CBA' };
+classes[2] = { name: 'Hunter', color: '#ABD473' };
+classes[3] = { name: 'Rogue', color: '#FFF569' };
+classes[4] = { name: 'Priest', color: '#FFFFFF' };
+classes[5] = { name: 'Death Knight', color: '#C41F3B' };
+classes[6] = { name: 'Shaman', color: '#0070DE' };
+classes[7] = { name: 'Mage', color: '#40C7EB' };
+classes[8] = { name: 'Warlock', color: '#8787ED' };
+classes[9] = { name: 'Monk', color: '#00FF96' };
+classes[10] = { name: 'Druid', color: '#FF7D0A' };
+classes[11] = { name: 'Demon Hunter', color: '#A330C9' };
+
+var dummy1 = {
+    name: 'Test1',
+    class: 1
+};
+
+var dummy2 = {
+    name: 'Test2',
+    class: 2
+};
+
+var data = [dummy1, dummy2];
